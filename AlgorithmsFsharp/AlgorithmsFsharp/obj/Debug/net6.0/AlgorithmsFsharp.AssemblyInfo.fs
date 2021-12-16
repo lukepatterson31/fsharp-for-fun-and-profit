eee1e386d8caa6801e074f87f434ac92b35296e2
Luke Patterson <lukepatterson31@gmail.com>
2021-12-16 11:32:13 +0000

    -

--- AlgorithmsFsharp/AlgorithmsFsharp/obj/Debug/net6.0/AlgorithmsFsharp.AssemblyInfo.fs
+++ AlgorithmsFsharp/AlgorithmsFsharp/obj/Debug/net6.0/AlgorithmsFsharp.AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System
 open System.Reflection
 
 
-[<assembly: System.Reflection.AssemblyCompanyAttribute("FsharpWebApi")>]
+[<assembly: System.Reflection.AssemblyCompanyAttribute("AlgorithmsFsharp")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
-[<assembly: System.Reflection.AssemblyProductAttribute("FsharpWebApi")>]
-[<assembly: System.Reflection.AssemblyTitleAttribute("FsharpWebApi")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("AlgorithmsFsharp")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("AlgorithmsFsharp")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
 do()
